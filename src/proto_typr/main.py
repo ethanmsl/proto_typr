e394b620068f711488d847ef4b6db093b89296d9
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-01-18 08:15:48 -0800

    feat: cwd print & print from specified file

--- src/proto_typr/main.py
+++ src/proto_typr/main.py
@@ -3,6 +3,8 @@ Contains the bulk of top-level execution code.
 However execution occurs in `__main__.py`
 """
 
+import os
+
 import typer
 
 app = typer.Typer()
@@ -25,6 +27,20 @@ def goodbye(name: str, formal: bool = False) -> None:
         print(f"Goodbye {name}. It was a pleasure.")
     else:
         print(f"Later {name}.")
+
+
+@app.command()
+def print_rows(location: str) -> None:
+    """Print rows from a file"""
+    with open(location, encoding="utf8") as file:
+        for row in file:
+            print(row)
+
+
+@app.command()
+def print_cwd() -> None:
+    """Print the current working directory"""
+    print(os.getcwd())
 
 
 def junk_function_for_coverage_report() -> None:
