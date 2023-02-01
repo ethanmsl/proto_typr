28d34f6b6baa13aa27af322229b7b7ca9ecf72fe
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-01-31 19:55:37 -0800

    removal of eol whitespace

--- src/proto_typr/__init__.py
+++ src/proto_typr/__init__.py
@@ -1,5 +1,6 @@
 """
-This is the nominal landing page, but Jekyl is using the `index.html` file instead.  
+This is the nominal landing page, but Jekyl is using the `index.html` file instead.
+
 
 .. include:: ../../README.md
 """
