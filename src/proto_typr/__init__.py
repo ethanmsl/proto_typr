d8c358a101839cc56739bed9b6287f8652bae6b7
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-01-31 21:58:53 -0800

    build: disabling pylint trailing-whitespace to allow markdown formatting
    Black already cleans up trailing-whitespace outside of docstrings

--- src/proto_typr/__init__.py
+++ src/proto_typr/__init__.py
@@ -1,6 +1,17 @@
 """
 This is the nominal landing page, but Jekyl is using the `index.html` file instead.
-
+Look  
+Now  
+I'
+ve  
+got  
+s
+o
+m
+e
+trail  
+ing  
+whitespace  
 
 .. include:: ../../README.md
 """
