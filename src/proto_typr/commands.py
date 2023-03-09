8358cea3c476311e4962c626177d79f2a9e527b3
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-03-09 11:14:44 -0800

    feat: get cwd and write to labelled value BUT ovewrites whole config

--- src/proto_typr/commands.py
+++ src/proto_typr/commands.py
@@ -30,6 +30,20 @@ app = typer.Typer(
 APP_NAME = "proto-typr"
 
 
+@app.command("confwrite")
+def write_to_config(dir_num: int = typer.Argument(..., min=0)) -> None:
+    """write current working dir to config file"""
+    app_dir = typer.get_app_dir(APP_NAME)
+    config_path: Path = Path(app_dir) / "config.json"
+
+    if not config_path.is_file():
+        rprint(f"Config file not found at {config_path}")
+        raise typer.Abort()
+
+    cwd = Path.cwd()
+    config_path.write_text(f"working_dir_{dir_num}: {str(cwd)}")
+
+
 @app.command("read_conf")
 def read_config() -> Optional[str]:
     """Read from the config file, if it exists"""
